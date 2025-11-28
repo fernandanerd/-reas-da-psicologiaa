c73fbc846b94ad1300aea014aebb92d4a42ef2c0
fernandanerd <nandamita0202@gmail.com>
2025-11-28 23:11:51 +0000

    aaaaaaaa

--- js/script.js
+++ js/script.js
@@ -4,10 +4,20 @@ function validarForm() {
     let senha = document.getElementById("senha").value;
     let erro = document.getElementById("erro");
 
+    // Usuário e senha definidos
+    let usuarioCorreto = "admin";
+    let emailCorreto = "admin@gmail.com";
+    let senhaCorreta = "1234";
+
     if (username === "" || email === "" || senha === "") {
-        alert('Preencha todos os campos!');
-    } else {
-        erro.textContent = ""; // limpa erro
+        erro.textContent = "Preencha todos os campos!";
+        return;
+    }
+
+    if (username === usuarioCorreto && email === emailCorreto && senha === senhaCorreta) {
+        erro.textContent = "";
         window.location.href = "index.html"; 
+    } else {
+        erro.textContent = "Usuário, email ou senha incorretos!";
     }
 }
